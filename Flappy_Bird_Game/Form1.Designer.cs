ab4fa35540a3d98442aed2c0959940c9c87da263
yousef <ywseft2006@gmail.com>
2024-10-24 21:41:11 +0300

    Add project files.

---
+++ Flappy_Bird_Game/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Flappy_Bird_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            picBird = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            picColumn = new PictureBox();
+            picColumnx = new PictureBox();
+            picColumnX2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)picBird).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picColumn).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picColumnx).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picColumnX2).BeginInit();
+            SuspendLayout();
+            // 
+            // picBird
+            // 
+            picBird.BackColor = Color.Transparent;
+            picBird.Image = Properties.Resources.bird;
+            picBird.Location = new Point(316, 303);
+            picBird.Name = "picBird";
+            picBird.Size = new Size(125, 60);
+            picBird.SizeMode = PictureBoxSizeMode.StretchImage;
+            picBird.TabIndex = 0;
+            picBird.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 16;
+            timer1.Tick += timer1_Tick;
+            // 
+            // picColumn
+            // 
+            picColumn.BackColor = Color.Transparent;
+            picColumn.Image = Properties.Resources.column2;
+            picColumn.Location = new Point(842, 490);
+            picColumn.Name = "picColumn";
+            picColumn.Size = new Size(139, 650);
+            picColumn.SizeMode = PictureBoxSizeMode.StretchImage;
+            picColumn.TabIndex = 1;
+            picColumn.TabStop = false;
+            // 
+            // picColumnx
+            // 
+            picColumnx.BackColor = Color.Transparent;
+            picColumnx.Image = Properties.Resources.column2x;
+            picColumnx.Location = new Point(842, -286);
+            picColumnx.Name = "picColumnx";
+            picColumnx.Size = new Size(139, 551);
+            picColumnx.SizeMode = PictureBoxSizeMode.StretchImage;
+            picColumnx.TabIndex = 2;
+            picColumnx.TabStop = false;
+            // 
+            // picColumnX2
+            // 
+            picColumnX2.BackColor = Color.Transparent;
+            picColumnX2.Image = Properties.Resources.column2x;
+            picColumnX2.Location = new Point(622, -315);
+            picColumnX2.Name = "picColumnX2";
+            picColumnX2.Size = new Size(139, 551);
+            picColumnX2.SizeMode = PictureBoxSizeMode.StretchImage;
+            picColumnX2.TabIndex = 3;
+            picColumnX2.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Desktop;
+            ClientSize = new Size(984, 687);
+            Controls.Add(picColumnX2);
+            Controls.Add(picColumnx);
+            Controls.Add(picColumn);
+            Controls.Add(picBird);
+            DoubleBuffered = true;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            KeyDown += Form1_KeyDown;
+            ((System.ComponentModel.ISupportInitialize)picBird).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picColumn).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picColumnx).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picColumnX2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox picBird;
+        private System.Windows.Forms.Timer timer1;
+        private PictureBox picColumn;
+        private PictureBox picColumnx;
+        private PictureBox picColumnX2;
+    }
+}
---
+++ Flappy_Bird_Game/Form1.cs
@@ -0,0 +1,104 @@
+﻿namespace Flappy_Bird_Game
+{
+    public partial class Form1 : Form
+    {
+        bool IsGameActive = true;
+        int pepesShow = 0;
+
+        //show the pepes after start game
+        private void ShowPepes()
+        {
+            picColumn.Visible = true;
+            picColumnx.Visible = true;
+            picColumnX2.Visible = true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            picColumn.Visible = false;
+            picColumnx.Visible = false;
+            picColumnX2.Visible = false;
+        }
+
+        //check the Collision and Game Over
+        private bool CheckCollision()
+        {
+
+            if (picBird.Bounds.IntersectsWith(picColumn.Bounds) ||
+                picBird.Bounds.IntersectsWith(picColumnx.Bounds) ||
+                picBird.Bounds.IntersectsWith(picColumnX2.Bounds) ||
+                picBird.Bounds.Y < -50 ||
+                picBird.Bounds.Y > this.Height)
+            {
+                IsGameActive = false;   // game over
+                timer1.Enabled = false; //stop the game
+                MessageBox.Show("GAME OVER");
+            }
+            return IsGameActive;
+        }
+
+
+        // reset the pepe(columns ) location
+        private void ResetColumn()
+        {
+
+            Random rand = new Random();
+
+            int x = rand.Next(930, 1000);
+            int y = rand.Next(-350, -300);
+            int bottomY = rand.Next(410, 430);// this for bottom pepe
+
+            if (picColumn.Left < -50) // column
+                picColumn.Location = new Point(x, bottomY);
+
+            if (picColumnx.Left < -50) //columnx
+                picColumnx.Location = new Point(x, y);
+
+            if (picColumnX2.Left < -50)
+                picColumnX2.Location = new Point((x -90), (y+75)); //new pepe(columnX2)
+
+            //if (picColumnX2.Left < -30)
+            //    HiedePepes();
+        }
+
+
+        //the columns function left to right
+        private void columnAction()
+        {
+            picColumn.Left -= 6;
+            picColumnx.Left -= 6;
+            picColumnX2.Left -= 6; //new pepe(columnX2)
+
+        }
+
+        //timer
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CheckCollision();
+            ResetColumn(); // function
+            columnAction(); //function
+            picBird.Top += 10; //bird is fall
+
+        }
+
+        //when pressing the space (jump)
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            timer1.Enabled = IsGameActive;  //if user pressig space start the game
+            if (IsGameActive)
+                picBird.Top -= 60; // jump
+
+            if (pepesShow == 0)
+            {
+                ShowPepes();
+                pepesShow = 1;
+            }
+        }
+
+        // when start game show the pepes and close the timer2
+        
+    }
+}
